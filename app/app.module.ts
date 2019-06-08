b8c6ca55e687ba8c661ec17c2b64a60bc2914849
Nele Taevere <ntaevere@gmail.com>
2019-06-09 05:12:43 +1000

    update prettier settings

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,17 +1,11 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { BrowserModule } from "@angular/platform-browser";
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
 
 @NgModule({
-  imports: [
-    BrowserModule
-  ],
-  bootstrap: [
-    AppComponent
-  ],
-  declarations: [
-    AppComponent
-  ]
+  imports: [BrowserModule, CommonModule],
+  declarations: [AppComponent],
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
