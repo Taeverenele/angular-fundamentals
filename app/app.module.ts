c465440e06a362387fa918717afe10cd7321aa4b
Nele Taevere <ntaevere@gmail.com>
2019-06-09 05:27:36 +1000

    remove test styles

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,7 +1,7 @@
-import { AppComponent } from "./app.component";
-import { BrowserModule } from "@angular/platform-browser";
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { AppComponent } from "./app.component";
 
 @NgModule({
   imports: [BrowserModule, CommonModule],
