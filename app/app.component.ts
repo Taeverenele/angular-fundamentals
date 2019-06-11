27b94ef012966ca89166dcef9958ca5ed0bff213
Nele Taevere <ntaevere@gmail.com>
2019-06-11 19:30:55 +1000

    use ngFor to iterate over a list of passengers

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,13 +1,37 @@
 import { Component } from "@angular/core";
 
+interface Passenger {
+  id: number,
+  fullname: string,
+  checkedIn: boolean
+}
+
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.scss"],
 })
+
 export class AppComponent {
-  public name: string = "";
-  public handleChange(val: string) {
-    this.name = val;
-  }
+  passengers: Passenger[] = [{
+    id: 1,
+    fullname: 'Stephen',
+    checkedIn: true
+  }, {
+    id: 2,
+    fullname: 'Rose',
+    checkedIn: false
+  }, {
+    id: 3,
+    fullname: 'James',
+    checkedIn: true
+  }, {
+    id: 4,
+    fullname: 'Louise',
+    checkedIn: true
+  }, {
+    id: 5,
+    fullname: 'Tina',
+    checkedIn: false
+  }];
 }
