d9a89f5c0a1813714a0668117cda3b72eaa7face
Nele Taevere <ntaevere@gmail.com>
2019-06-12 20:01:46 +1000

    create passenger-count and passenger detail dumb components and add to passenger-dashboard module

---
+++ app/passenger-dashboard/components/passenger-count/passenger-count.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "passenger-count",
+  template: `<div>Passenger count</div>`,  // styleUrls: ["./passenger-count.component.scss"],
+})
+
+export class PassengerCountComponent {
+}
