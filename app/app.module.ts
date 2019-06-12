04250920879990d3302db89fb292e3a4661ed646
Nele Taevere <ntaevere@gmail.com>
2019-06-12 19:17:12 +1000

    create passenger-dashboard custom module

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,9 +2,14 @@ import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { AppComponent } from "./app.component";
+import { PassengerDashboardModule } from "./passenger-dashboard/passenger-dashboard.module";
 
 @NgModule({
-  imports: [BrowserModule, CommonModule],
+  imports: [
+    // angular modules
+    BrowserModule, CommonModule,
+    // custom modules
+    PassengerDashboardModule],
   declarations: [AppComponent],
   bootstrap: [AppComponent],
 })
---
+++ app/passenger-dashboard/passenger-dashboard.module.ts
@@ -0,0 +1,10 @@
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+
+@NgModule({
+  imports: [BrowserModule, CommonModule],
+  declarations: [],
+  bootstrap: [],
+})
+export class PassengerDashboardModule {}
