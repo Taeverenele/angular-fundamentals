fa205da1d83b1deb27e5d54f39028bb64a4cf23f
Nele Taevere <ntaevere@gmail.com>
2019-06-12 19:36:26 +1000

    create passenger dashboard custom container component and move passengers logic and template over from app.component

--- app/passenger-dashboard/passenger-dashboard.module.ts
+++ app/passenger-dashboard/passenger-dashboard.module.ts
@@ -1,10 +1,12 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
+import { PassengerDashboardComponent } from "./containers/passenger-dashboard/passenger-dashboard.component";
 
 @NgModule({
   imports: [BrowserModule, CommonModule],
-  declarations: [],
+  declarations: [PassengerDashboardComponent],
+  exports: [PassengerDashboardComponent],
   bootstrap: [],
 })
 export class PassengerDashboardModule {}
