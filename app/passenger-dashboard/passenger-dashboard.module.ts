069c6736939387da12ea6df6e1e26139ca1698a1
Nele Taevere <ntaevere@gmail.com>
2019-06-16 19:18:46 +1000

    create passenger-dashboard.service and getPassengers method to return list of passengers

--- app/passenger-dashboard/passenger-dashboard.module.ts
+++ app/passenger-dashboard/passenger-dashboard.module.ts
@@ -1,14 +1,15 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
-import { PassengerDashboardComponent } from "./containers/passenger-dashboard/passenger-dashboard.component";
-import { PassengerDetailComponent } from "./components/passenger-detail/passenger-detail.component";
 import { PassengerCountComponent } from "./components/passenger-count/passenger-count.component";
+import { PassengerDetailComponent } from "./components/passenger-detail/passenger-detail.component";
+import { PassengerDashboardComponent } from "./containers/passenger-dashboard/passenger-dashboard.component";
+import { PassengerDashboardService } from "./passenger-dashboard.service";
 
 @NgModule({
   imports: [BrowserModule, CommonModule],
   declarations: [PassengerDashboardComponent, PassengerDetailComponent, PassengerCountComponent],
   exports: [PassengerDashboardComponent],
-  bootstrap: [],
+  providers: [PassengerDashboardService],
 })
 export class PassengerDashboardModule {}
