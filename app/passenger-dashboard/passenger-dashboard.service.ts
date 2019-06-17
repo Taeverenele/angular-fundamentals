e0e9e26e011f9bc274e0bb96680c11d9df498c5d
Nele Taevere <ntaevere@gmail.com>
2019-06-17 19:43:57 +1000

    add example of how to create custom headers and RequestOptions in PassengerDashboardService

--- app/passenger-dashboard/passenger-dashboard.service.ts
+++ app/passenger-dashboard/passenger-dashboard.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from "@angular/core";
-import { Http } from "@angular/http";
+import { Http, Headers, RequestOptions } from "@angular/http";
 import { Observable } from "rxjs/Observable";
 import "rxjs/add/operator/map";
 import { Passenger } from "./models/passenger.interface";
@@ -19,14 +19,21 @@ export class PassengerDashboardService {
   }
 
   public updatePassenger(passenger: Passenger): Observable<Passenger> {
-    return this.http.put(`${PASSENGER_API}/${passenger.id}`, passenger).map(
-      (response) => response.json()
+    // Headers and request options not needed for this http request
+    // but here to show how can/should be done
+    const headers = new Headers({
+      'Content-Type': 'application/json'
+    });
+
+    const options = new RequestOptions({ headers });
+    return this.http.put(`${PASSENGER_API}/${passenger.id}`, passenger, options).map(
+      (response) => response.json(),
     );
   }
 
   public removePassenger(passenger: Passenger): Observable<Passenger> {
     return this.http.delete(`${PASSENGER_API}/${passenger.id}`).map(
-      (response) => response.json()
+      (response) => response.json(),
     );
   }
 }
